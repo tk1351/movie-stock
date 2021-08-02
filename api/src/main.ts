dad28deb46d64494fed385cde46f991c481cabd7
tk1351 <takakikitsui0111@gmail.com>
2021-08-02 17:28:54 +0900

    process.env.PORT追加

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(8080, '0.0.0.0');
+  await app.listen(process.env.PORT || 8080, '0.0.0.0');
 }
 bootstrap();
