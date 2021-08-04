301303efd16d30904bb023264025906a9cc2d365
tk1351 <takakikitsui0111@gmail.com>
2021-08-04 14:42:37 +0900

    MoviesListへ無限スクロール追加、レイアウト修正

--- app/components/movie/MoviesList.tsx
+++ app/components/movie/MoviesList.tsx
@@ -1,27 +1,47 @@
-import React, { useEffect } from 'react'
-import { useRecoilState, useRecoilValueLoadable } from 'recoil'
+import React, { useEffect, useState } from 'react'
+import {
+  useRecoilState,
+  useRecoilValueLoadable,
+  useSetRecoilState,
+  useRecoilStateLoadable,
+} from 'recoil'
 import { NextPage } from 'next'
 import { useAuth0 } from '@auth0/auth0-react'
+import InfiniteScroll from 'react-infinite-scroller'
+import { Grid, Typography, Box } from '@material-ui/core'
 import { authState } from '../../recoil/atoms/auth'
 import { moviesState, watchedState } from '../../recoil/atoms/movie'
-import { fetchMovies, fetchWatchedNumber } from '../../src/utils/api/movie'
+import {
+  fetchMoviesByUser,
+  fetchWatchedNumber,
+} from '../../src/utils/api/movie'
 import { registerUser } from '../../src/utils/api/user'
 import { RegisterUser } from '../../types/user'
 import MovieItem from './MovieItem'
+import API from '../../src/utils/api/api'
+import { IMovie } from '../../types/movie'
+import { fetchMovies } from '../../recoil/selectors/movie'
+import Spinner from '../common/Spinner'
+import styles from '../../styles/components/movie/moviesList.module.css'
 
 interface MoviesListPageProps {}
 
 const MoviesList: NextPage<MoviesListPageProps> = () => {
   const accessToken = useRecoilValueLoadable(authState)
-  const [movies, setMovies] = useRecoilState(moviesState)
+  const [movies, setMovies] = useRecoilStateLoadable(moviesState)
   const [watched, setWatched] = useRecoilState(watchedState)
+  const setIsFetched = useSetRecoilState<IMovie[]>(fetchMovies)
+
+  const [hasMore, setHasMore] = useState(true)
 
   const { user } = useAuth0()
 
   useEffect(() => {
     ;(async () => {
       if (accessToken.state === 'hasValue') {
-        const moviesData = await fetchMovies(accessToken.contents.accessToken)
+        const moviesData = await fetchMoviesByUser(
+          accessToken.contents.accessToken
+        )
         const watchedNumber = await fetchWatchedNumber(
           accessToken.contents.accessToken
         )
@@ -41,12 +61,45 @@ const MoviesList: NextPage<MoviesListPageProps> = () => {
     await registerUser(userData)
   }
 
+  const loadMore = async () => {
+    const limit: number = 30
+
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/movies?offset=${movies.contents.length}&limit=${limit}`
+    const res = await API.get<IMovie[]>(url)
+
+    try {
+      setIsFetched([...movies.contents, ...res.data])
+    } finally {
+      setHasMore(false)
+    }
+  }
+
+  const loader = <Spinner key={0} />
+
   return (
     <div>
+      <Grid container justifyContent="center" className={styles.h2}>
+        {watched && (
+          <Typography gutterBottom variant="h4" component="h2">
+            <Box fontWeight="fontWeightBold">鑑賞本数: {watched}</Box>
+          </Typography>
+        )}
+      </Grid>
+      <InfiniteScroll loadMore={loadMore} hasMore={hasMore} loader={loader}>
+        <Grid container spacing={2} className={styles.list}>
+          <Grid item xs={2} />
+          <Grid item xs={8}>
+            <Grid container spacing={10}>
+              {movies.state === 'hasValue' &&
+                movies.contents.map((movie) => (
+                  <MovieItem key={movie.id} movie={movie} />
+                ))}
+            </Grid>
+          </Grid>
+          <Grid item xs={2} />
+        </Grid>
+      </InfiniteScroll>
       <button onClick={() => onRegisterUserClicked()}>登録</button>
-      <h1>映画一覧</h1>
-      {watched && <h2>鑑賞本数: {watched}</h2>}
-      <MovieItem movies={movies} />
     </div>
   )
 }
