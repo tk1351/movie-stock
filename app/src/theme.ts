ae78747f918c90efb74b315138c04fa420a3d9a5
tk1351 <takakikitsui0111@gmail.com>
2021-08-04 14:42:08 +0900

    ダークテーマへ変更

--- app/src/theme.ts
+++ app/src/theme.ts
@@ -1,5 +1,9 @@
 import { unstable_createMuiStrictModeTheme as createTheme } from '@material-ui/core/styles'
 
-const theme = createTheme({})
+const theme = createTheme({
+  palette: {
+    type: 'dark',
+  },
+})
 
 export default theme
