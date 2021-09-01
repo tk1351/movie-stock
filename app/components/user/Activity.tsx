96d35b278ed8d2ed17c06b904e7501dd8963ad60
tk1351 <takakikitsui0111@gmail.com>
2021-09-01 15:16:47 +0900

    ActivityとMyPageのリファクタリング

--- app/components/user/Activity.tsx
+++ app/components/user/Activity.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { NextPage } from 'next'
-import Link from 'next/link'
 import {
   TableContainer,
   Paper,
   Table,
-  TableHead,
   TableRow,
   TableBody,
   TableCell,
 } from '@material-ui/core'
-import styles from '../../styles/components/user/activity.module.css'
+import LinkTable from '../common/LinkTable'
+import { activityRows } from '../../src/utils/tableRow'
 
 interface ActivityProps {
   watched: number
@@ -18,15 +17,6 @@ interface ActivityProps {
 
 const Activity: NextPage<ActivityProps> = ({ watched }) => {
   const rows = [{ index: 1, name: '鑑賞本数', number: watched }]
-
-  const activityRows = [
-    { index: 1, category: '監督', route: 'directors' },
-    { index: 2, category: '脚本', route: 'writers' },
-    { index: 3, category: '製作', route: 'producers' },
-    { index: 4, category: '撮影', route: 'photographers' },
-    { index: 5, category: '製作国', route: 'countries' },
-    { index: 6, category: '制作会社', route: 'studios' },
-  ]
 
   return (
     <>
@@ -44,26 +34,7 @@ const Activity: NextPage<ActivityProps> = ({ watched }) => {
           </TableBody>
         </Table>
       </TableContainer>
-      {activityRows.map((activityRow) => (
-        <TableContainer
-          component={Paper}
-          className={styles.tableContainer}
-          key={activityRow.index}
-        >
-          <Table className={styles.link}>
-            <Link href={`/user/${activityRow.route}`}>
-              <TableHead className={styles.tableHead}>
-                <TableRow>
-                  <TableCell component="th" scope="row">
-                    {activityRow.category}
-                  </TableCell>
-                  <TableCell component="th" align="right"></TableCell>
-                </TableRow>
-              </TableHead>
-            </Link>
-          </Table>
-        </TableContainer>
-      ))}
+      <LinkTable contents={activityRows} />
     </>
   )
 }
