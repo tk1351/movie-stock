e1c08b5e74d66dc917510a9b356bda3056464fdc
tk1351 <takakikitsui0111@gmail.com>
2021-09-06 11:16:21 +0900

    fix: Sortのデザイン

--- app/components/common/Sort.tsx
+++ app/components/common/Sort.tsx
@@ -1,50 +1,31 @@
 import React, { FC } from 'react'
-import {
-  TableContainer,
-  Paper,
-  Table,
-  TableRow,
-  TableBody,
-  TableCell,
-  Typography,
-  Box,
-} from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 import styles from '../../styles/components/common/sort.module.css'
 import SortButton, { SortCategoryComponentType } from './SortButton'
 
 interface SortProps {
-  watched: number
   category: SortCategoryComponentType
   query?: string
   begin?: number
   end?: number
 }
 
-const Sort: FC<SortProps> = ({ watched, category, query, begin, end }) => {
+const Sort: FC<SortProps> = ({ category, query, begin, end }) => {
   return (
-    <TableContainer component={Paper} className={styles.wrapper}>
-      <Table>
-        <TableBody>
-          <TableRow key={1}>
-            <TableCell component="th" scope="row">
-              {watched && (
-                <Typography gutterBottom variant="h4" component="h2">
-                  <Box fontWeight="fontWeightBold">鑑賞本数: {watched}</Box>
-                </Typography>
-              )}
-            </TableCell>
-            <TableCell align="right">
-              <SortButton
-                category={category}
-                query={query}
-                begin={begin}
-                end={end}
-              />
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Grid container spacing={2}>
+      <Grid item xs={2} />
+      <Grid item xs={8}>
+        <Grid container className={styles.sortButton}>
+          <SortButton
+            category={category}
+            query={query}
+            begin={begin}
+            end={end}
+          />
+        </Grid>
+      </Grid>
+      <Grid item xs={2} />
+    </Grid>
   )
 }
 
