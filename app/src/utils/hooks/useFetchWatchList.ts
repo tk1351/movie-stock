9a788ebdc0359eb3cc8c30a11d5b885727f96217
tk1351 <takakikitsui0111@gmail.com>
2021-09-08 07:56:24 +0900

    feat: watchListをAPIで取得する部分のCustom Hooks化

---
+++ app/src/utils/hooks/useFetchWatchList.ts
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react'
+import {
+  useRecoilValueLoadable,
+  useRecoilState,
+  useRecoilStateLoadable,
+  Loadable,
+} from 'recoil'
+import { authState } from '../../../recoil/atoms/auth'
+import { IWatchList } from '../../../types/watchList'
+import { watchListState } from '../../../recoil/atoms/watchList'
+import { watchedState } from '../../../recoil/atoms/movie'
+import API, { offset, limit } from '../api/api'
+import { setAuthToken } from '../api/setAuthToken'
+
+type useFetchWatchListReturnType = [Loadable<IWatchList[]>, number, boolean]
+
+export const useFetchWatchList = (): useFetchWatchListReturnType => {
+  const accessToken = useRecoilValueLoadable(authState)
+  const [data, setData] = useRecoilStateLoadable<IWatchList[]>(watchListState)
+  const [watched, setWatched] = useRecoilState(watchedState)
+
+  const [isLoading, setIsLoading] = useState(true)
+
+  const fetchWatchListAndLength = async (accessToken: string) => {
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/watch-list?offset=${offset}&limit=${limit}`
+    setAuthToken(accessToken)
+    const res = await API.get<[IWatchList[], number]>(url)
+
+    const watchList = res.data[0]
+    const length = res.data[1]
+
+    setData(watchList)
+    setWatched(length)
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    if (accessToken.state === 'hasValue') {
+      fetchWatchListAndLength(accessToken.contents.accessToken)
+    }
+  }, [accessToken])
+
+  return [data, watched, isLoading]
+}
