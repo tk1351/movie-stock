c1d91ad64e39366bd338a387dc6d6a37267bc016
tk1351 <takakikitsui0111@gmail.com>
2021-09-08 13:42:11 +0900

    fix: Custom Hooksの呼び出しをListコンポーネントに変更

--- app/components/watch-list/List.tsx
+++ app/components/watch-list/List.tsx
@@ -1,25 +1,31 @@
 import React, { FC } from 'react'
 import { Grid, Typography, Box } from '@material-ui/core'
-import { IWatchList } from '../../types/watchList'
 import ListTable from './ListTable'
+import { useFetchWatchList } from '../../src/utils/hooks/useFetchWatchList'
+import Spinner from '../common/Spinner'
 
-interface ListProps {
-  watchList: IWatchList[]
-}
+interface ListProps {}
 
-const List: FC<ListProps> = ({ watchList }) => {
+const List: FC<ListProps> = () => {
+  const [data, isLoading] = useFetchWatchList()
   return (
     <>
-      <Grid container>
-        <Grid container justifyContent="center">
-          <Typography gutterBottom variant="h4" component="h2">
-            <Box fontWeight="fontWeightBold">
-              観たい映画: {watchList.length}本
-            </Box>
-          </Typography>
-        </Grid>
-      </Grid>
-      <ListTable watchList={watchList} />
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Grid container>
+            <Grid container justifyContent="center">
+              <Typography gutterBottom variant="h4" component="h2">
+                <Box fontWeight="fontWeightBold">
+                  観たい映画: {data.contents.length}本
+                </Box>
+              </Typography>
+            </Grid>
+          </Grid>
+          <ListTable />
+        </>
+      )}
     </>
   )
 }
