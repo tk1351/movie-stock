367176d9195da0885c990075cfc93bb51d3bf210
tk1351 <takakikitsui0111@gmail.com>
2021-09-08 15:00:55 +0900

    fix: 観たい映画の本数がDBから返るよう、watchedを返り値に追記

--- app/src/utils/hooks/useFetchWatchList.ts
+++ app/src/utils/hooks/useFetchWatchList.ts
@@ -3,18 +3,21 @@ import {
   useRecoilValueLoadable,
   useRecoilStateLoadable,
   Loadable,
+  useRecoilState,
 } from 'recoil'
 import { authState } from '../../../recoil/atoms/auth'
 import { IWatchList } from '../../../types/watchList'
 import { watchListState } from '../../../recoil/atoms/watchList'
 import API, { offset, limit } from '../api/api'
 import { setAuthToken } from '../api/setAuthToken'
+import { watchedState } from '../../../recoil/atoms/movie'
 
-type useFetchWatchListReturnType = [Loadable<IWatchList[]>, boolean]
+type useFetchWatchListReturnType = [Loadable<IWatchList[]>, number, boolean]
 
 export const useFetchWatchList = (): useFetchWatchListReturnType => {
   const accessToken = useRecoilValueLoadable(authState)
   const [data, setData] = useRecoilStateLoadable<IWatchList[]>(watchListState)
+  const [watched, setWatched] = useRecoilState(watchedState)
 
   const [isLoading, setIsLoading] = useState(true)
 
@@ -24,8 +27,10 @@ export const useFetchWatchList = (): useFetchWatchListReturnType => {
     const res = await API.get<[IWatchList[], number]>(url)
 
     const watchList = res.data[0]
+    const count = res.data[1]
 
     setData(watchList)
+    setWatched(count)
     setIsLoading(false)
   }
 
@@ -35,5 +40,5 @@ export const useFetchWatchList = (): useFetchWatchListReturnType => {
     }
   }, [accessToken])
 
-  return [data, isLoading]
+  return [data, watched, isLoading]
 }
