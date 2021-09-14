a4e204f3f203a4c20ec03610665307998bdb95b9
tk1351 <takakikitsui0111@gmail.com>
2021-09-15 07:50:50 +0900

    feat: DBからのタイトル、監督名検索、リセットボタン、ボタンのdisable

--- app/components/watch-list/SearchWatchListForm.tsx
+++ app/components/watch-list/SearchWatchListForm.tsx
@@ -1,12 +1,19 @@
-import React, { FC } from 'react'
-import { useSetRecoilState } from 'recoil'
-import { useForm, SubmitHandler, Controller } from 'react-hook-form'
+import React, { FC, useMemo, useState } from 'react'
+import { useSetRecoilState, useRecoilValueLoadable } from 'recoil'
+import { useForm, Controller } from 'react-hook-form'
 import { TextField, Button, Container } from '@material-ui/core'
+import debounce from 'lodash.debounce'
 import {
   WatchListFormReturnType,
   watchListFormState,
 } from '../../recoil/atoms/open'
 import styles from '../../styles/components/watch-list/searchForm.module.css'
+import { Auth, authState } from '../../recoil/atoms/auth'
+import API, { offset, limit } from '../../src/utils/api/api'
+import { setAuthToken } from '../../src/utils/api/setAuthToken'
+import { IWatchList } from '../../types/watchList'
+import { watchListState } from '../../recoil/atoms/watchList'
+import { Clear } from '@material-ui/icons'
 
 interface SearchWatchListFormProps {}
 
@@ -15,15 +22,15 @@ interface ISearchQuery {
 }
 
 const SearchWatchListForm: FC<SearchWatchListFormProps> = () => {
-  const setForm = useSetRecoilState<WatchListFormReturnType>(watchListFormState)
+  const [input, setInput] = useState<boolean>(false)
 
-  const { control, handleSubmit } = useForm<ISearchQuery>({
+  const setForm = useSetRecoilState<WatchListFormReturnType>(watchListFormState)
+  const accessToken = useRecoilValueLoadable<Auth>(authState)
+  const setWatchList = useSetRecoilState<IWatchList[]>(watchListState)
+
+  const { control, reset } = useForm<ISearchQuery>({
     defaultValues: { query: '' },
   })
-
-  const onSubmit: SubmitHandler<ISearchQuery> = async (data) => {
-    console.log('data', data)
-  }
 
   const onClick = () => {
     setForm((prev) => ({
@@ -31,29 +38,83 @@ const SearchWatchListForm: FC<SearchWatchListFormProps> = () => {
       category: undefined,
     }))
   }
+
+  const fetchWatchList = async (url: string) => {
+    try {
+      if (accessToken.state === 'hasValue')
+        setAuthToken(accessToken.contents.accessToken)
+      const res = await API.get<[IWatchList[], number]>(url)
+      const result = res.data[0]
+
+      setWatchList([...result])
+    } catch (e) {
+      throw new Error(e)
+    }
+  }
+
+  const fetchWatchListByQuery = async (query: string) => {
+    const url = `${
+      process.env.NEXT_PUBLIC_API_URL
+    }/watch-list?offset=${offset}&limit=${limit}&query=${encodeURI(query)}`
+
+    await fetchWatchList(url)
+  }
+
+  const debouncedFetchWatchListByQuery = useMemo(
+    () => debounce((query: string) => fetchWatchListByQuery(query), 800),
+    []
+  )
+
+  const clearInputValue = async () => {
+    reset({ query: '' })
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/watch-list?offset=${offset}&limit=${limit}`
+
+    setInput(false)
+    await fetchWatchList(url)
+  }
+
+  const preventSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
+
+  const isInputted = (query: string) => {
+    if (query.length === 0) setInput(false)
+    return setInput(true)
+  }
+
   return (
     <Container
       component="main"
       maxWidth={false}
       className={styles.searchFormWrapper}
     >
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={preventSubmit}>
         <Controller
           name="query"
           control={control}
-          render={({ field: { onChange } }) => (
+          render={({ field: { onChange, value } }) => (
             <TextField
               label="検索"
               id="search"
               type="text"
               name="query"
               variant="outlined"
-              onChange={onChange}
+              value={value}
+              defaultValue=""
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                onChange(e.target.value)
+                debouncedFetchWatchListByQuery(e.target.value)
+                isInputted(e.target.value)
+              }}
             />
           )}
         />
-        <Button type="submit" color="primary" variant="contained">
-          検索
+        <Button
+          type="button"
+          disabled={input === false}
+          onClick={clearInputValue}
+        >
+          <Clear fontSize="small" />
         </Button>
         <Button
           type="button"
