14007f7636ec64d355e568232391e4c1ac80ac56
tk1351 <takakikitsui0111@gmail.com>
2021-09-15 14:51:10 +0900

    fix: ListTableでSpinnerを表示するよう修正

--- app/components/watch-list/List.tsx
+++ app/components/watch-list/List.tsx
@@ -2,7 +2,6 @@ import React, { FC } from 'react'
 import { Grid, Typography, Box } from '@material-ui/core'
 import ListTable from './ListTable'
 import { useFetchWatchList } from '../../src/utils/hooks/useFetchWatchList'
-import Spinner from '../common/Spinner'
 
 interface ListProps {}
 
@@ -10,20 +9,14 @@ const List: FC<ListProps> = () => {
   const [data, watched, isLoading] = useFetchWatchList()
   return (
     <>
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <>
-          <Grid container>
-            <Grid container justifyContent="center">
-              <Typography gutterBottom variant="h4" component="h2">
-                <Box fontWeight="fontWeightBold">観たい映画: {watched}本</Box>
-              </Typography>
-            </Grid>
-          </Grid>
-          <ListTable watchList={data} />
-        </>
-      )}
+      <Grid container>
+        <Grid container justifyContent="center">
+          <Typography gutterBottom variant="h4" component="h2">
+            <Box fontWeight="fontWeightBold">観たい映画: {watched}本</Box>
+          </Typography>
+        </Grid>
+      </Grid>
+      <ListTable watchList={data} isLoading={isLoading} />
     </>
   )
 }
