4f636e9dbaafe5eff17fec23f4494ba1da388e93
tk1351 <takakikitsui0111@gmail.com>
2021-09-17 14:52:43 +0900

    fix: memo化、updateボタン追加

--- app/components/watch-list/EditMenu.tsx
+++ app/components/watch-list/EditMenu.tsx
@@ -1,7 +1,8 @@
-import React, { FC } from 'react'
+import React, { FC, memo } from 'react'
+import { useRouter } from 'next/router'
 import { Menu, MenuItem } from '@material-ui/core'
 import { useRecoilState, useSetRecoilState } from 'recoil'
-import { Edit, Delete } from '@material-ui/icons'
+import { Edit, Delete, Update } from '@material-ui/icons'
 import {
   menuState,
   dialogState,
@@ -12,9 +13,11 @@ import { currentColumnState } from '../../recoil/atoms/watchList'
 
 interface EditMenuProps {}
 
-const EditMenu: FC<EditMenuProps> = () => {
+const EditMenu: FC<EditMenuProps> = memo(() => {
+  const router = useRouter()
+
   const [anchorEl, setAnchorEl] = useRecoilState<null | HTMLElement>(menuState)
-  const setCurrentColumn = useSetRecoilState<IWatchList | null>(
+  const [currentColumn, setCurrentColumn] = useRecoilState<IWatchList | null>(
     currentColumnState
   )
   const setOpen = useSetRecoilState<DialogStateReturnType>(dialogState)
@@ -28,22 +31,37 @@ const EditMenu: FC<EditMenuProps> = () => {
     setOpen({ open: true, category })
     setAnchorEl(null)
   }
+
+  const onPushRegisterPageClicked = (id: number) => {
+    setAnchorEl(null)
+    if (currentColumn) router.push(`/update/watch-list/${currentColumn.id}`)
+  }
+
   return (
-    <Menu
-      id="edit-menu"
-      open={Boolean(anchorEl)}
-      onClose={handleClose}
-      keepMounted
-      anchorEl={anchorEl}
-    >
-      <MenuItem>
-        <Edit onClick={() => handleClickOpen('edit')} />
-      </MenuItem>
-      <MenuItem>
-        <Delete onClick={() => handleClickOpen('delete')} />
-      </MenuItem>
-    </Menu>
+    <>
+      {currentColumn && (
+        <Menu
+          id="edit-menu"
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+          keepMounted
+          anchorEl={anchorEl}
+        >
+          <MenuItem>
+            <Update
+              onClick={() => onPushRegisterPageClicked(currentColumn.id)}
+            />
+          </MenuItem>
+          <MenuItem>
+            <Edit onClick={() => handleClickOpen('edit')} />
+          </MenuItem>
+          <MenuItem>
+            <Delete onClick={() => handleClickOpen('delete')} />
+          </MenuItem>
+        </Menu>
+      )}
+    </>
   )
-}
+})
 
 export default EditMenu
