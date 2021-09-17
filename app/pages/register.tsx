8d47ce80a5f830f6209eb2c7d1c1fffec13dfd85
tk1351 <takakikitsui0111@gmail.com>
2021-09-17 14:50:39 +0900

    fix: watchListから登録もできるように、ページで読み込むコンポーネントの変更

--- app/pages/register.tsx
+++ app/pages/register.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NextPage } from 'next'
 import Head from 'next/head'
 import Navbar from '../components/common/Navbar'
-import RegisterForm from '../components/movie/RegisterForm'
+import Register from '../components/movie/Register'
 
 const register: NextPage = () => {
   return (
@@ -11,7 +11,7 @@ const register: NextPage = () => {
         <title>映画の登録 | CineStock</title>
       </Head>
       <Navbar />
-      <RegisterForm />
+      <Register />
     </>
   )
 }
